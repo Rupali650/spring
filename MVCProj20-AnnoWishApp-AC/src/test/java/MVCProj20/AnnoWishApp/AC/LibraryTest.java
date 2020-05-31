31d9da485395cafe520f3a74d62b7ad97bbc36f1
“Rupali650” <rupaliswain015@gmail.com>
2020-05-31 18:43:28 +0530

    add dependency

--- MVCProj20-AnnoWishApp-AC/src/test/java/MVCProj20/AnnoWishApp/AC/LibraryTest.java
+++ MVCProj20-AnnoWishApp-AC/src/test/java/MVCProj20/AnnoWishApp/AC/LibraryTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.*;
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
+        @Autowired
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
